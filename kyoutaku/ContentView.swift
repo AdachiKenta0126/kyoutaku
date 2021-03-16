ab7fee134520e14c9c6abb54f59ea06f2a391345
AdachiKenta <k18004kk@aitech.ac.jp>
2021-03-17 01:14:15 +0900

    Initial Commit

---
+++ kyoutaku/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  kyoutaku
+//
+//  Created by k18004kk on 2021/03/17.
+//  Copyright © 2021 AIT. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
